4cf3d3c6cae92e96b377af92a0983a11cd4bb7f6
Nousheen Ali <nousheen@Nousheens-MacBook-Air.local>
2022-10-04 23:23:10 +0400

    module6 ex00

--- Module 06/ex00/Converter.cpp
+++ Module 06/ex00/Converter.cpp
@@ -3,16 +3,87 @@
 /*                                                        :::      ::::::::   */
 /*   Converter.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nali <nali@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: nali <nali@42abudhabi.ae>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 12:32:54 by nali              #+#    #+#             */
-/*   Updated: 2022/10/04 12:33:57 by nali             ###   ########.fr       */
+/*   Updated: 2022/10/04 23:21:27 by nali             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Converter.hpp"
 
+
+void Converter::checkType()
+{
+    int l;
+    std::string substr;
+    
+    l = str.length();
+    if (l == 1 &&  isalpha(str[0]))
+    {
+        std::cout <<"char type"<<std::endl;
+        this->type = CharType;
+        this->val_char = str[0];
+        return ;
+    }
+    else if (l == 1 && !isdigit(str[0])) // for cases like +, - , .
+    {
+        this->type = UnknownType;
+        std::cout <<"Unknown type"<<std::endl;
+        return ;
+    }
+    else 
+    {
+        if (str[0] == '-' || str[0] == '+')
+        {
+            substr = str.substr(1,l - 1);
+            l--;
+        }
+        else
+            substr = str;
+        if (substr.find_first_not_of("0123456789") == std::string::npos) //checks if only digits present
+        {
+            this->type = IntType;
+            std::cout <<"Integer type"<<std::endl;
+            return ;
+        }
+        if (substr.find('.') != std::string::npos && (substr.find_first_of(".") == substr.find_last_of("."))) //checks if decimal present and only one present
+        {
+            if (substr.find_first_not_of(".0123456789") == std::string::npos && substr[l - 1] != '.')
+            {
+                this->type = DoubleType;
+                std::cout <<"Double type"<<std::endl;
+                return ;
+            }
+            else if (substr[l - 1] == 'f' && (substr.find_first_of("f") == substr.find_last_of("f"))) //checks if last char is 'f' and only one char present
+            {
+                this->type = FloatType;
+                std::cout <<"Float type"<<std::endl;
+                return ;
+            }
+            else
+            {
+                this->type = UnknownType;
+                std::cout <<"Unknown type"<<std::endl;
+                return ;
+            }  
+        }
+        else
+        {
+            this->type = UnknownType;
+            std::cout <<"Unknown type"<<std::endl;
+            return ;
+        }  
+    }
+}
+    
+    
+
+
 Converter::Converter(std::string str)
 {
-    
+    this->str = str;
+    checkType();
 }
+
+
--- Module 06/ex00/Converter.hpp
+++ Module 06/ex00/Converter.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Converter.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nali <nali@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: nali <nali@42abudhabi.ae>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 12:28:17 by nali              #+#    #+#             */
-/*   Updated: 2022/10/04 12:34:14 by nali             ###   ########.fr       */
+/*   Updated: 2022/10/04 21:37:02 by nali             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,28 @@
 #define CONVERTER_H
 
 #include <string>
+#include <iostream>
 
 class Converter
 {
-    
+    private:
+        std::string str;
+        int type;
+        char val_char;
+        // int val_int;
+        // float val_float;
+        // double val_double;
+        
+        enum Type{CharType, IntType, FloatType, DoubleType, UnknownType};
+        
     public:
-        Converter();
-        ~Converter();
+        // Converter();
+        // ~Converter();
         Converter(std::string str);
-        Converter(const Converter &old_obj);
-        Converter& operator=(const Converter &obj)
+        // Converter(const Converter &old_obj);
+        // Converter& operator=(const Converter &obj);
+
+        void checkType();
 };
 
 #endif
